97147f00701635d01bbe8cc0ac10e77589f96d66
TsHristov <tsvetanhristov@yahoo.com>
2017-10-08 01:03:00 +0300

    Add range_sum

--- Week1/practice.scm
+++ Week1/practice.scm
@@ -1,5 +1,13 @@
-;; Define function that checks if
+;; Define a function that checks if
 ;; a number is even:
 
 (define (even x)
   (=(remainder x 2) 0))
+
+;; Define a function that finds the
+;; sum of number in a given range [x,y]
+
+(define (range_sum x y)
+  (if (>= x y)
+      x
+      (+ x (range_sum (+ x 1) y)))) 
