56d762afb4cb40413b1d167ef5e476a434602829
TsHristov <tsvetanhristov@yahoo.com>
2017-10-08 10:32:12 +0300

    Add biggest_squares_sum

--- Week1/practice.scm
+++ Week1/practice.scm
@@ -2,10 +2,27 @@
 ;; a number is even:
 
 (define (even x)
-  (=(remainder x 2) 0))
+  (= (remainder x 2) 0))
 
+;; Define a function that accepts three arguments
+;; and returns the sum of the squares of the larger two:
+
+(define (square x) ( * x x))
+
+(define (sum_squares x y)
+  (+ (square x) (square y)))
+
+(define (smaller x y z)
+  (and (< x y) (< x z)))
+
+(define (biggest_squares_sum x y z)
+  (cond
+   ((smaller x y z) (sum_squares y z))
+   ((smaller y x z) (sum_squares x z))
+   (else (sum_squares x y))))
+     
 ;; Define a function that finds the
-;; sum of number in a given range [x,y]
+;; sum of number in a given range [x,y]:
 
 (define (range_sum x y)
   (if (>= x y)
