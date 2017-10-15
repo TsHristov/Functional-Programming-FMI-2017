d65b8e5fd8c7ef3164840c51683b8e0467c9c6e3
TsHristov <tsvetanhristov@yahoo.com>
2017-10-15 22:04:59 +0300

    Fix kth element solution

--- 99-Scheme-Problems/03.scm
+++ 99-Scheme-Problems/03.scm
@@ -1,12 +1,10 @@
 ;; Find the K`th element of a list:
-(define (kth_element list_argument k)
-  (cond
-   ((<= k 0) "Invalid index!")
-   (else kth list_argument k 1)))
 
-(define (kth list_argument k index)
-  (cond
-   ((null? (cdr list_argument)) "none found")
-   ((= index k) (car list_argument))
-   ((< index k)
-          (kth (cdr list_argument) k (+ index 1)))))
+(define (kth l k)
+  (if (and (> k 0) (<= k (length l)) (list? l))
+      (kth-element l k 1)))
+
+(define (kth-element l k index)
+  (if (= index k)
+      (car l)
+      (kth-element (cdr l) k (+ 1 index))))
