24e09bf55d666d638c5fbb171b708ab4b148e00e
TsHristov <tsvetanhristov@yahoo.com>
2017-11-11 16:06:16 +0200

    Add flatten and cartesian-product functions

--- Practice/Week5/practice.scm
+++ Practice/Week5/practice.scm
@@ -1,24 +1,37 @@
-;; member*: Check if element is a member of a list:
-(define (member* x l)
+;; member?: Check if element is a member of a list:
+;; Example:
+;;     (member? 2 (list 1 3 4)) -> #f
+;;     (member? 2 (list 3 3 2)) -> #t
+(define (member? x l)
   (if (null? l) #f
-      (or (= x (car l)) (member* x (cdr l)))))
+      (or (= x (car l)) (member? x (cdr l)))))
 
 ;; length*: Find the length of a list:
+;; Example:
+;;     (length* (list 1 2 3)) -> 3
+;;     (length* (list)) -> 0
 (define (length* l)
   (if (null? l) 0
       (+ 1 (length* (cdr l)))))
 
 ;; append*: Append two lists:
+;; Example:
+;;     (append* (list 2 3) (list 3 4)) -> (2 3 3 4)
 (define (append* l1 l2)
   (if (null? l2) l1
       (cons (car l1) (car l2) (append* l1 (cdr l2)))))
 
 ;; reverse*: Reverse a list:
+;; Example:
+;;     (reverse* (list 1 2 3)) -> (3 2 1)
 (define (reverse* l)
   (if (null? l) l
       (append (reverse* (cdr l)) (list (car l)))))
 
 ;; union*: Find the union of two lists:
+;; Example:
+;;     (union* (list 1 2 3) (list 2 2 2 4)) -> (1 2 3 4)
+;;     (union* '() (list 2 3 4)) -> (2 3 4)
 (define (union* l1 l2)
   (define appended (append l1 l2))
   (define (unify appended res)
@@ -30,6 +43,9 @@
   (unify appended '()))cons
 
 ;; intersection*: Find the intersection of two lists:
+;; Example:
+;;     (intersection* (list 1 2 3) (list 2 4 5)) -> (2)
+;;     (intersection* (list 1 2 3) (list 4 5 6)) -> ()
 (define (intersection* l1 l2)
   (define (helper l1 l2 res)
     (if (null? l1) res
@@ -39,6 +55,9 @@
   (helper l1 l2 '()))
 
 ;; slice*: Slice a given list from index i to index j:
+;; Example:
+;;     (slice* (list 1 2 3 4) 0 3) -> (1 2 3 4)
+;;     (slice* (list 1 2 3) 1 1) -> (2)
 (define (slice* l i j)
   (if (> i j) '())
   (define (slice l n)
@@ -50,6 +69,9 @@
   (slice l 0))
 
 ;; any?: Returns #t if the predicate p? applies to any of the elements of the list l:
+;; Example:
+;;     (any? even? (list 1 2 3)) -> #t
+;;     (any? odd? (list 2 4)) -> #f
 (define (accumulate l f initial)
   (if (null? l) initial
       (accumulate (cdr l) f (f initial (car l)))))
@@ -58,14 +80,17 @@
   (accumulate l (lambda (initial x) (or (p? x) initial)) #f))
 
 ;; all?: Returns #t if the predicate p? applies to all of the elements of the list l:
+;; Example:
+;;     (all? integer? (list 1 2 3)) -> #t
+;;     (all? even? (list 1 2 3)) -> #f
 (define (all? p? l)
   (accumulate l (lambda (initial x) (and (p? x) initial)) #t))
 
 ;; member-deep?: Finds an element x in a nested list structure:
 ;; Example:
-;;      (member-deep? 2 '((1 (3)) ((1 (1 (1 (2))))))) -> #t
+;;     (member-deep? 2 '((1 (3)) ((1 (1 (1 (2))))))) -> #t
+(define (atom? l) (and (not (null? l)) (not (pair? l))))
 (define (member-deep? x l)
-  (define (atom? l) (and (not (null? l)) (not (pair? l))))
   (cond
    ((null? l) #f)
    ((atom? l) (equal? x l))
@@ -73,5 +98,24 @@
 	     (member-deep? x (cdr l))))))
 
 ;; flatten: Flattens a nested lists:
-;; cartesion-product: Example: (1 2 3) (3 4) -> ((1 3) (1 4) (2 3) (2 4) ... )
+;; Example:
+;;     (flatten '((1 (2) (3)) (((((4))))))) -> (1 2 3 4)
+(define (flatten l)
+  (cond
+   ((null? l) '())
+   ((atom? l) (list l))
+   (else (append (flatten (car l)) (flatten (cdr l))))))
+
+;; cartesion-product: Returns the cartesian-product of two lists:
+;; Example:
+;;     (cartesian-product (list 1 2 3) (list 3 4)) -> ((1 3) (1 4) (2 3) (2 4) (3 3) (3 4))
+(define (cartesian-product l1 l2)
+  (define l2-copy l2)
+  (define (helper l1 l2-copy)
+    (cond
+     ((null? l1) '())
+     ((null? l2-copy) (helper (cdr l1) l2))
+     (else (append (list (list (car l1) (car l2-copy)))
+		   (helper l1 (cdr l2-copy))))))
+  (helper l1 l2-copy))
 
