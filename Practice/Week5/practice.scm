862938ff13ecae9e323322966edcf75ec3ecc600
TsHristov <tsvetanhristov@yahoo.com>
2017-11-11 13:12:51 +0200

    Add union and slice functions

--- Practice/Week5/practice.scm
+++ Practice/Week5/practice.scm
@@ -1,25 +1,35 @@
-;; member*:
+;; member*: Check if element is a member of a list:
 (define (member* x l)
   (if (null? l) #f
       (or (= x (car l)) (member* x (cdr l)))))
 
-;; length*:
+;; length*: Find the length of a list:
 (define (length* l)
   (if (null? l) 0
       (+ 1 (length* (cdr l)))))
 
-;; append*:
+;; append*: Append two lists:
 (define (append* l1 l2)
   (if (null? l2) l1
       (cons (car l1) (car l2) (append* l1 (cdr l2)))))
 
-;; reverse*:
+;; reverse*: Reverse a list:
 (define (reverse* l)
   (if (null? l) l
       (append (reverse* (cdr l)) (list (car l)))))
 
-;; union*:
-;; intersection*:
+;; union*: Find the union of two lists:
+(define (union* l1 l2)
+  (define appended (append l1 l2))
+  (define (unify appended res)
+    (cond
+     ((null? appended) res)
+     ((not (member* (car appended) res))
+      (unify (cdr appended) (append res (list (car appended)))))
+     (else (unify (cdr appended) res))))
+  (unify appended '()))cons
+
+;; intersection*: Find the intersection of two lists:
 (define (intersection* l1 l2)
   (define (helper l1 l2 res)
     (if (null? l1) res
@@ -28,10 +38,20 @@
 	  (helper (cdr l1) l2 res))))
   (helper l1 l2 '()))
 
-;; slice* list index1 index2:
-;; any? list:
-;; all? list:
+;; slice*: Slice a given list from index i to index j:
+(define (slice* l i j)
+  (if (> i j) '())
+  (define (slice l n)
+    (cond
+     ((null? l) l)
+     ((< n i) (slice (cdr l) (+ n 1)))
+     ((<= n j) (cons (car l) (slice (cdr l) (+ n 1))))
+     ((> n j) '())))
+  (slice l 0))
+
+;; any?:
+;; all?:
 ;; member-deep?: Finds en element x in a nested lists
-;; flatten:
+;; flatten: Flattens a nested lists:
 ;; cartesion-product: Example: (1 2 3) (3 4) -> ((1 3) (1 4) (2 3) (2 4) ... )
 
