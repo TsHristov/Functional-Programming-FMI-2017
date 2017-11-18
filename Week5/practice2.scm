ad9c5f459823bbeadee2bdbc2299a9083767daf8
TsHristov <tsvetanhristov@yahoo.com>
2017-11-18 10:40:17 +0200

    Add uniques, extract-ints, insert-at and insert-in-sorted functions, modify nth to make use of take and rop

--- Week5/practice2.scm
+++ Week5/practice2.scm
@@ -2,12 +2,25 @@
   (if (> a b) nv
       (op (term a) (accumulate op nv (next a) b term next))))
 
-;; nth: Takes the n-th element of a given list:
-(define (nth n lst)
+(define (filter* l p?)
   (cond
-   ((null? lst) '())
-   ((= n 0) (car lst))
-   (else (nth (- n 1) (cdr lst)))))
+   ((null? l) l)
+   ((p? (car l)) (cons (car l) (filter* (cdr l) p?)))
+   (else (filter* (cdr l) p?))))
+
+;; take: Take the first n elements of a given list:
+(define (take n lst)
+  (if (or (= n 0) (null? lst)) '()
+      (cons (car lst) (take (- n 1) (cdr lst)))))
+
+;; drop: Drop the first n elements of a given list:
+(define (drop n lst)
+  (if (= n 0) lst
+      (drop (- n 1) (cdr lst))))
+
+;; nth: Takes the n-th element of a given list:
+(define (nth n l)
+  (take 1 (drop (- n 1) l)))
 
 ;; range: Returns list of the numbers in the interval [from; to]:
 (define (range from to)
@@ -21,17 +34,38 @@
      (else (cons (remainder n 10) (digits (quotient n 10))))))
   (reverse* (digits n)))
 
-;; take: Take the first n elements of a given list:
-(define (take n lst)
-  (if (or (= n 0) (null? lst)) '()
-      (cons (car lst) (take (- n 1) (cdr lst)))))
+;; zip: Zips two lists:
+(define (zip l1 l2)
+  (if (or (null? l1) (null? l2)) '()
+      (cons (cons (car l1) (car l2)) (zip (cdr l1) (cdr l2)))))
 
-;; drop: Drop the first n elements of a given list:
-(define (drop n lst)
-  (if (= n 0) lst
-      (drop (- n 1) (cdr lst))))
-	      
+;; zip-with: Zips two lists with applied function:
+(define (zip-with f l1 l2)
+  (if (or (null? l1) (null? l2)) '()
+      (cons (f (car l1) (car l2)) (zip-with f (cdr l1) (cdr l2)))))
+
 ;; sorted: Checks if the array is in ascending order:
 (define (sorted l)
   (if (or (null? l) (null? (cdr l))) #t
       (and (<= (car l) (cadr l)) (sorted (cdr l)))))
+
+;; uniques: Returns the unique elements of a list:
+(define (uniques l)
+  (cond
+   ((null? l) '())
+   ((not (member (car l) (cdr l))) (cons (car l) (uniques (cdr l))))
+   (else (uniques (cdr l)))))
+
+;; extract-ints: Returns list of ints from a given list with arbitrary objects:
+(define (extract-ints l)
+  (filter* l (lambda (x) (integer? x))))
+
+;; insert-at: Inserts a value in at a given position in the list:
+(define (insert-at n value l)
+  (append (take n l) (list value) (drop n l)))
+
+;; insert-in-sorted: Inserts a value in it`s proper position in sorted array
+(define (insert-in-sorted val l)
+  (append (filter* l (lambda (x) (< x val)))
+	  (list val)
+	  (filter* l (lambda (x) (>= x val)))))
