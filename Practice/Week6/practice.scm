478e175b21a045f3a3a5b2d25bb82db8c2976d34
TsHristov <tsvetanhristov@yahoo.com>
2017-11-20 23:50:16 +0200

    Add nth-column and diagonal functions

--- Practice/Week6/practice.scm
+++ Practice/Week6/practice.scm
@@ -1,7 +1,7 @@
 (load "../../Common/common_functions.scm")
 
-;; map*: Map via foldr:
-(define (map* f l)
+;; map**: Map via foldr:
+(define (map** f l)
   (fold-right (lambda (x nv) (cons (f x) nv)) '() l))
 
 ;; reverse*: Reverse via foldr:
@@ -17,8 +17,37 @@
 (define (matrix-sum matrix)
   (sum (map (lambda (x) (sum x)) matrix)))
 
-;; matrix-min: Find the minimum element of a matrix:
+;; matrix-min: Finds the minimum element of a matrix:
 (define (matrix-min matrix)
   (define (min x y) (if (< x y) x y))
   (define (min-el l) (fold-right min (car l) l))
   (min-el (map min-el matrix)))
+
+;; get-nth: Takes the nth element of a list:
+(define (get-nth n l)
+  ;; take: Take the first n elements of a list:
+  (define (take n l)
+    (if (or (= n 0) (null? l)) '()
+	(cons (car l) (take (- n 1) l))))
+  ;; drop: Drop the first n elements of a list:
+  (define (drop n l)
+    (if (or (= n 0) (null? l)) l
+	(drop (- n 1) (cdr l))))
+  (take 1 (drop (- n 1) l)))
+
+;; transpose: Transpose a given matrix:
+(define (transpose matrix) (apply map list matrix))
+
+;; nth-column: Gets the nth column of a given matrix:
+(define (nth-column nth matrix)
+  (get-nth nth (transpose matrix)))
+
+;; diagonal: Gets the diagonal of a given matrix:
+(define (from-to a b)
+  (accumulate-right cons '() a b (lambda (x) x) (lambda (x) (+ x 1))))
+
+(define (diagonal matrix)
+  (let ((indices (from-to 1 (length (car matrix)))))
+    (map (lambda (row) (get-nth (car x) (cdr x)))
+	 (apply map list indices matrix))))
+
