9ede7f6799870add04abbe9a09dde5dbe8aca22e
TsHristov <tsvetanhristov@yahoo.com>
2017-12-08 16:31:08 +0200

    Add add-edge, path-exists? and predecessors functions

---
+++ Week8/practice2.scm
@@ -0,0 +1,42 @@
+;; vertices: Return the verices of a given graph:
+(define (vertices graph)
+  (map car graph))
+
+;; successors: Return the successors of a given vertex in a graph:
+(define (successors vertex graph)
+  (cdr (assoc vertex graph)))
+
+;; has-edge?: Check if path exists between vertex u and vertex v (directed graph):
+(define (has-edge? u v graph)
+  (and (member v (successors u graph)) #t))
+
+;; add-vertex: Add new vertex to the graph:
+(define (add-vertex vertex graph)
+  (if (assoc vertex graph) graph
+      (cons (list vertex) graph)))
+
+;; add-edge: Add new edge between two vertices (directed graph):
+(define (add-edge u v graph)
+  (cond
+   ((has-edge? u v graph) graph)
+   ((not (member v (vertices graph)))
+    (map (lambda (x) (if (equal? u (car x))
+			 (append x (list v))
+			 x))
+	 (add-vertex v graph)))
+   (else (map (lambda (x) (if (equal? u (car x))
+			      (append x (list v))
+			      x)))
+	 graph)))
+
+;; path-exists?: Check if the given path of vertices exists in the graph:
+(define (path-exists? path graph)
+  (if (not (null? (cdr path)))
+      (and (has-edge? (car path) (cadr path) graph)
+	   (path-exists? (cdr path) graph))
+      (and (member (car path) (vertices graph)) #t)))
+
+;; predecessors: Find all vertices in the graph which are predecessors of the given:
+(define (predecessors vertex graph)
+  (vertices (filter (lambda (x) (member vertex (successors (car x) graph))) graph)))
+
