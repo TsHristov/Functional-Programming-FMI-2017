7013645167f0bbae6861e90c2007576de2233dc4
TsHristov <tsvetanhristov@yahoo.com>
2017-12-22 20:15:07 +0200

    Add solutions to practice tasks

--- Practice/Week9/practice.hs
+++ Practice/Week9/practice.hs
@@ -34,19 +34,16 @@ pack list
 
 -- 5. Encode:
 --    Example: encode "aaaabcc" => [(4, 'a'), (1, 'b'), (2, 'c')]
-
 encode :: Eq a => [a] -> [(Int, a)]
 encode list = map (\x -> (length x, head x)) (pack list)
 
 -- 6. Decode:
 --    Example: decode [(4, 'a'), (1, 'b'), (2, 'c')] => "aaaabcc"
-
 decode :: [(Int, a)] -> [[a]]
 decode = map (\x -> take (fst x) (repeat (snd x)))
 
 -- 7. Replicate:
 --    Example: replicate 3 2 => [2, 2, 2]
-
 replicate' :: Int -> a -> [a]
 replicate'       0 _ = []
 replicate' n element = element : replicate' (n-1) element
@@ -54,9 +51,25 @@ replicate' n element = element : replicate' (n-1) element
 replicate'' :: Int -> a -> [a]
 replicate'' times element = take times (repeat element)
 
--- 8. Sublist:
---    Example: sublist 0 2 [1, 2, 3, 4] => [1, 2]
-sublist' a b list = drop a (take b list)
+-- 8. Split:
+--    Example: split 2 [1,2,3,4,5] -> ([1,2], [3,4,5])
+split :: Int -> [a] -> ([a], [a])
+split index list = (take index list, drop index list)
 
--- 9. Rotate:
+-- 9. Slice: Returns a sublist of a given list from index a to index b:
+--    Example: sublist 0 2 [1, 2, 3, 4] => [1, 2]
+slice a b list = drop a (take b list)
+
+-- 10. Rotate:
 --    Example: rotate "abcdefgh" 3 => "defghabc"
+rotate :: [a] -> Int -> [a]
+rotate string n = (drop count string) ++ (take count string)
+  where count = if n > 0 then n else length string + n
+ 
+-- 11. Prime factors: Finds all prime factors of a given number:
+isPrime :: Int -> Bool
+isPrime number = null [ x | x <- [2..number-1], number `mod` x == 0 ] && not (number `elem` [0,1])
+
+primeFactors :: Int -> [Int]
+primeFactors number = [ x | x <- [1..number], number `mod` x == 0, isPrime x ]
+
