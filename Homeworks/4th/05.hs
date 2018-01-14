b8b27c68a1cbc4756dc11eae6d4ff402d0ba0a5d
TsHristov <tsvetanhristov@yahoo.com>
2018-01-14 23:48:10 +0200

    Add Traveling Salesman Naive Solution in Haskell

---
+++ Homeworks/4th/05.hs
@@ -0,0 +1,29 @@
+import Data.List
+
+-- distance: Finds distance between two points
+distance :: (Float, Float) -> (Float, Float) -> Float
+distance (x1,y1) (x2,y2) = sqrt $ (x2 - x1) ** 2 + (y2 - y1) ** 2
+
+-- totalDistance: Finds the distance between series of points:
+totalDistance :: [(Float,Float)] -> Float
+totalDistance  [] = 0
+totalDistance [x] = 0
+totalDistance (x:y:xs) = (distance x y) + totalDistance (y:xs)
+
+-- minDistance: Sorting criterion
+minDistance :: Ord a => (c, a) -> (b, a) -> Ordering
+minDistance (_, x2) (_, y2)
+  | x2 < y2   = LT
+  | x2 > y2   = GT
+  | otherwise = EQ
+
+-- minRoute: Extract the minimum distance from (Path, Distance) pair
+minRoute :: [(a,b)] -> b
+minRoute = snd . head
+
+-- minPath: Traveling Salesman Problem (Naive Solution)
+minPath :: (Float, Float) -> [(Float,Float)] -> Float
+minPath home places  = minRoute minRoutes
+  where paths        = map (\place -> [home] ++ place ++ [home]) (permutations places)
+        distances    = map (\x -> (x, totalDistance x)) paths
+        minRoutes    = sortBy minDistance distances
