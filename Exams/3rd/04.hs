0fbdac2bb9602db3c6b6f28e22daea97551b2e85
TsHristov <tsvetanhristov@yahoo.com>
2018-01-28 22:50:33 +0200

    Add exam task 4 solution

---
+++ Exams/3rd/04.hs
@@ -0,0 +1,16 @@
+type Name   = String
+type Length = Int
+type Video  = (Name, Length)
+
+videoName :: Video -> Name
+videoName   = fst
+
+videoLength :: Video -> Length 
+videoLength = snd
+
+-- averageVideo: Returns the video with length closest to the average length
+-- Example: averageVideo [("lolcat", 15), ("dogewow", 35), ("omgseethis", 28)] → "lolcat"
+averageVideo :: [Video] -> Name
+averageVideo [video]  = videoName video
+averageVideo videos   = videoName $ last $ filter (\video -> (videoLength video) <= averageLength ) videos
+  where averageLength = (sum $ map videoLength videos) `div` (fromIntegral $ Prelude.length videos)
