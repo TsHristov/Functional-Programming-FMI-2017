c084e43af7fb347a1fb49bc29025a90146298f5f
TsHristov <tsvetanhristov@yahoo.com>
2018-01-30 09:55:30 +0200

    Add exams solutionts

---
+++ Exams/3rd/02.hs
@@ -0,0 +1,21 @@
+-- Task 2:
+-- Transform a Binary Tree into new Binary Tree where the value of each root
+-- is tuple of the smallest interval contatining all values in the tree:
+
+data BinaryTree a = EmptyTree | Node { root  :: a,
+                                       left  :: (BinaryTree a),
+                                       right :: (BinaryTree a)
+                                     } deriving (Eq, Show)
+
+getValues :: BinaryTree a -> [a]
+getValues EmptyTree                    = []
+getValues (Node x EmptyTree EmptyTree) = [x]
+getValues (Node x l r)                 = getValues l ++ [x] ++ getValues r
+
+intervalTree :: BinaryTree Int -> BinaryTree [Int]
+intervalTree EmptyTree                       = EmptyTree
+intervalTree (Node root EmptyTree EmptyTree) = (Node [root, root] EmptyTree EmptyTree)
+intervalTree (Node x l r)                    = (Node (intervals) (intervalTree l) (intervalTree r))
+  where findMin   = minimum $ [x] ++ getValues l
+        findMax   = maximum $ [x] ++ getValues r
+        intervals = [findMin, findMax]
---
+++ Exams/3rd/03.hs
@@ -0,0 +1,2 @@
+sumOfSquares :: [Int]
+sumOfSquares = [ x | a <- [1..], b <- [1..a], let x = a^2 + b^2 ]
